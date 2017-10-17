e85958c0d8529d5de72001f0f6f71909df37d9a5
sebatian003peru <sebastian003peru@gmail.com>
2017-10-17 16:52:10 -0500

    Created MainMenu and Added Start and Option Buttons

---
+++ Novox/Assets/MenuManager.cs
@@ -0,0 +1,14 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour {
+
+
+	public void LoadGame (string SceneName) {
+
+		SceneManager.LoadScene(SceneName);
+		
+	}
+}
