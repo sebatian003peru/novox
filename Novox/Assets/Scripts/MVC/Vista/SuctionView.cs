185750fc739f7b6ca28eaccd6e65486222e1e4cd
giussepe <giussepe_1999_1@hotmail.com>
2017-11-14 16:44:30 -0500

    Se comienza el desarrollo de View
    SuctionView se agrega y se modifica el Hud del score para que no se
    mologre los numeros de highScore y score

--- Novox/Assets/Scripts/MVC/Vista/SuctionView.cs
+++ Novox/Assets/Scripts/MVC/Vista/SuctionView.cs
@@ -2,7 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SuctionView : MonoBehaviour
+public class SuctionView : SuctionController 
 {
-	
+	void OnTriggerStay (Collider obj)
+	{
+		if (obj.gameObject.tag == "Player") 
+		{
+			PlayerHerfloat +=0.2f;
+			
+		}
+	}
+	void OnTriggerExit (Collider obj)
+	{
+		if (obj.gameObject.tag == "Player") 
+		{
+			PlayerHerfloat = -1;
+		}
+	}
 }
